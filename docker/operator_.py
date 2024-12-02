11b7952fd612a8d16ae11584eacb7a98d855600e
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-12-02 17:41:35 +0100

    Add Ruff

--- docker/operator_.py
+++ docker/operator_.py
@@ -23,6 +23,7 @@ def _hash(spec: kopf.Spec) -> str:
 
 @kopf.on.startup()
 def startup(settings: kopf.OperatorSettings, logger: kopf.Logger, **_: Any) -> None:
+    """Startup the operator."""
     settings.posting.level = logging.getLevelName(os.environ.get("LOG_LEVEL", "INFO"))
     if "KOPF_SERVER_TIMEOUT" in os.environ:
         settings.watching.server_timeout = int(os.environ["KOPF_SERVER_TIMEOUT"])
@@ -33,6 +34,7 @@ def startup(settings: kopf.OperatorSettings, logger: kopf.Logger, **_: Any) -> N
 
 
 def create_webhook(spec: kopf.Spec, logger: kopf.Logger) -> dict[str, Any]:
+    """Create the webhook."""
     webhooks_response = requests.get(
         f"https://api.github.com/repos/{spec['repository']}/hooks",
         headers={
@@ -90,6 +92,7 @@ def create_webhook(spec: kopf.Spec, logger: kopf.Logger) -> dict[str, Any]:
 
 
 def delete_webhook(url: str, repository: str, id_: int, logger: kopf.Logger) -> None:
+    """Delete the webhook."""
     result = requests.delete(
         f"https://api.github.com/repos/{repository}/hooks/{id_}",
         headers={
@@ -112,12 +115,14 @@ def delete_webhook(url: str, repository: str, id_: int, logger: kopf.Logger) -> 
 
 @kopf.on.create("camptocamp.com", "v4", f"githubwebhooks{ENVIRONMENT}")
 async def create(meta: kopf.Meta, spec: kopf.Spec, logger: kopf.Logger, **_: Any) -> dict[str, Any]:
+    """Manage the creation of the webhook."""
     logger.info("Create, Name: %s, Namespace: %s", meta.get("name"), meta.get("namespace"))
 
     return create_webhook(spec, logger)
 
 
 def get_status(status: kopf.Status) -> dict[str, Any]:
+    """Get the status of the webhook."""
     for name in ("update", "create"):
         if name in status:
             return cast(dict[str, Any], status[name])
@@ -129,6 +134,7 @@ def get_status(status: kopf.Status) -> dict[str, Any]:
 async def update(
     meta: kopf.Meta, spec: kopf.Spec, status: kopf.Status, logger: kopf.Logger, **_: Any
 ) -> Optional[dict[str, Any]]:
+    """Manage the update or resume of the webhook."""
     logger.info(
         "Update or resume, Name: %s, Namespace: %s",
         meta.get("name"),
@@ -147,6 +153,7 @@ async def update(
 async def delete(
     meta: kopf.Meta, spec: kopf.Spec, status: kopf.Status, logger: kopf.Logger, **_: Any
 ) -> None:
+    """Manage the deletion of the webhook."""
     logger.info(
         "Delete, Name: %s, Namespace: %s, Status: %s",
         meta.get("name"),
